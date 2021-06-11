4fd58882db69d927cbc2475f65ca101d25cd60a5
Shaheryar Shaikh <43124039+avc52github@users.noreply.github.com>
2021-06-11 19:59:33 +0530

    basics

--- app.js
+++ app.js
@@ -8,11 +8,33 @@ app.get("/",function(req,res){
     res.render("home");
 })
 
+app.get("/reg",function(req,res)
+{
+    res.send("register route");
+})
+
+app.get("/login",function(req,res)
+{
+    res.send("login route");
+})
+
+app.get("/subscribe",function(req,res){
+    res.send("offer route");
+})
+
+app.get("/upload",function(req,res)
+{
+    res.send("Upload form");
+})
+//this function get triggered when a user tries to access invalid route it return status of 404 not found and a webpage
+app.use(function (req, res) {
+    res.status(404).render('404');
+  });
 
 
-app.listen(3000,'localhost',function() {
-	// body...
-	console.log("Listening to port"+3000);
-	console.log("Clothesthenic Local Server has Started");
-
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen(port, host, function() {
+    console.log("Listening to port "+port);
+	console.log("CLothesthenic has Started");
 });
